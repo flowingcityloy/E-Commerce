9720e29f1e7c841ae21c6423d626cc307ea8784b
Lydia Loy <lydia_art@yahoo.com>
2021-12-17 22:35:45 -0600

    define models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -2,11 +2,27 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.INTEGER
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'reader', 
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
